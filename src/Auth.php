7e26c8bf6ab56d9925784e9dfc288e474fda81e6
changwei <867597730@qq.com>
2018-04-26 16:39:58 +0800

    验证增加函数验证，模块，控制器，操作验证规则支持通配符

--- src/Auth.php
+++ src/Auth.php
@@ -26,7 +26,17 @@ class Auth
     {
     }
 
-    public static function addPerm($strName, $strModule='index', $strController='index', $strAction='index', $strParameter='', $strRemark='')
+    public static function setup()
+    {
+//        TODO:改用Phinx\Migration迁移工具进行数据库初始化
+//        $strSqlFile = dirname(dirname(__FILE__)).'think_auth.sql';
+//        $strDDL = file_get_contents($strSqlFile);
+//        Db::execute($strDDL);
+    }
+
+    public static function addPerm($strName,
+                                   $strModule='index', $strController='index', $strAction='index', $strParameter='',
+                                   $strRuleFunction='', $strRemark='')
     {
         /*$objRole = new Perm;
         $objRole->perm_name = $strName;
@@ -44,6 +54,7 @@ class Auth
             'controller' => $strController,
             'action' => $strAction,
             'parameter' => $strParameter,
+            'rule_function' => $strRuleFunction,
             'remark' => $strRemark,
         ];
         return Db::table($strTableName)->insertGetId($arrData);
@@ -111,6 +122,7 @@ class Auth
         $strModule = $objRequest->module();
         $strController = $objRequest->controller();
         $strAction = $objRequest->action();
+//        TODO:参数校验，打算以正则表达式的方式进行
         $strParameter = $objRequest->param();
 
         $strTablePrefix = config('auth.database.table_prefix');
@@ -119,20 +131,42 @@ class Auth
         $strRolePermTableName = $strTablePrefix.config('auth.database.table_name.role_perm');
         $strPermTableName = $strTablePrefix.config('auth.database.table_name.perm');
 
-        $intRet = Db::table($strUserRoleTableName)->alias('ur')
+        $objTemp = Db::table($strUserRoleTableName)->alias('ur')
             ->join("$strRoleTableName r", 'ur.role_id = r.id')
             ->join("$strRolePermTableName rp", 'ur.role_id = rp.role_id')
             ->join("$strPermTableName p", 'rp.perm_id')
-            ->where('ur.user_id', '=', $intUserId)
-            ->where('p.module', '=',$strModule)
-            ->where('p.controller', '=',$strController)
-            ->where('p.action', '=',$strAction)
-//            ->where('p.parameter', '=', $strParameter)
-            ->count();
+            ->where('ur.user_id', '=', $intUserId);
+
+        $objTemp = $objTemp->where('p.module', '=', $strModule)
+            ->whereOr('p.module', '=', '*');
+        $intRet = $objTemp->count();
+//        如果用户所属的角色组规则在当前模块有“任意通配符”或者匹配当前模块成功则放行模块访问，否则禁止访问
         if ($intRet === 0) {
             return false;
-        } else {
+        }
+
+        $objTemp = $objTemp->where('p.controller', '=', $strController)
+            ->whereOr('p.controller', '=', '*');
+        $intRet = $objTemp->count();
+//        判断控制器规则，同上
+        if ($intRet === 0) {
+            return false;
+        }
+
+        $objTemp = $objTemp->where('p.action', '=', $strAction)
+            ->whereOr('p.action', '=', '*');
+        $intRet = $objTemp->count();
+//        判断操作方法规则，同上
+        if ($intRet === 0) {
+            return false;
+        }
+
+        $objTemp = $objTemp->find();
+        $strFunc = $objTemp->rule_function;
+        if (empty($strFunc)) {
             return true;
+        } else {
+            return call_user_func($strFunc);
         }
     }
 
